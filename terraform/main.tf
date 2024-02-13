41333adfaf7ade1ce1bd0107799e52789a450901
waheed <zamawaheed1506@gmail.com>
2024-02-14 03:52:01 +0530

    waheed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
-module "ecs_config" {
-  source = "./modules/ecs_config"
-  # Other configuration for ecs_config module
+data "aws_ecs_cluster" "existing_cluster" {
+  cluster_name = "my-ecs-cluster"  # Replace this with the name of your ECS cluster
 }
 
 module "task_definition" {
-  source = "./modules/task_definition"
-  ecs_cluster_id = module.ecs_config.ecs_cluster_id
+  source         = "./modules/task_definition"
+  ecs_cluster_id = data.aws_ecs_cluster.existing_cluster.id
   ecr_image_uri = var.ecr_image_uri
 }
