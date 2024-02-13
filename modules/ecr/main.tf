b88039cfac9f385af29b286f49b1505c96f355aa
waheed uz zama <108026280+waheed-15@users.noreply.github.com>
2024-02-13 22:00:53 +0530

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,12 +1,9 @@
 provider "aws" {
   region = var.region
 }
-
-
 resource "aws_ecr_repository" "app_repo" {
   name = var.ecr_name
 }
-
 output "ecr_repo_url" {
   value = aws_ecr_repository.app_repo.repository_url
 }
