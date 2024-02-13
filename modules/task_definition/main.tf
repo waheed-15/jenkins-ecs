6a133a515607e7f6d8659a22aa66ac73fc10a2be
waheed <zamawaheed1506@gmail.com>
2024-02-14 02:58:23 +0530

    waheed

--- modules/task_definition/main.tf
+++ modules/task_definition/main.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 
 resource "aws_ecs_service" "ecs_service" {
  name            = "my-ecs-service"
- cluster         = aws_ecs_cluster.ecs_cluster.id
+ cluster         = module.ecs_config.aws_ecs_cluster.ecs_cluster.id
  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
  desired_count   = 2
 
@@ -56,5 +56,5 @@ resource "aws_ecs_service" "ecs_service" {
    container_port   = 80
  }
 
- depends_on = [module.ecs_config.aws_autoscaling_group.ecs_asg]
+ depends_on = module.ecs_config.aws_autoscaling_group.ecs_asg
 }
