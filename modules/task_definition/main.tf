b75f4f0a4624d7feaa36d3534f6a986e87f55b42
waheed <zamawaheed1506@gmail.com>
2024-02-14 02:46:58 +0530

    waheed

--- modules/task_definition/main.tf
+++ modules/task_definition/main.tf
@@ -24,3 +24,37 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
    }
  ])
 }
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = ["subnet-0ab6ee277d0b523f8", "subnet-0c192bb582f2042c0"]
+   security_groups = ["sg-0498660e030d72814", "sg-067e56dd2d6651ddf"]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ triggers = {
+   redeployment = timestamp()
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+   container_name   = "dockergs"
+   container_port   = 80
+ }
+
+ depends_on = [module.ecs_config.aws_autoscaling_group.ecs_asg]
+}
