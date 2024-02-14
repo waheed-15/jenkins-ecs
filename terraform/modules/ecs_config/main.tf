21a08260db7a862c97f626c8675dc575ae5eec10
waheed <zamawaheed1506@gmail.com>
2024-02-14 16:31:17 +0530

    waheed

--- terraform/modules/ecs_config/main.tf
+++ terraform/modules/ecs_config/main.tf
@@ -70,10 +70,10 @@ resource "null_resource" "check_capacity_provider" {
   provisioner "local-exec" {
     command = <<EOT
       # Check if the capacity provider already exists
-      if grep -q '"capacity_provider_name": {"value": "test1"' /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfstate; then
-        echo "CAPACITY_PROVIDER_EXISTS=true" >> /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfvars
+      if grep -q '"capacity_provider_name"' /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfstate; then
+        echo "CAPACITY_PROVIDER_EXISTS= "true"" >> /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfvars
       else
-        echo "CAPACITY_PROVIDER_EXISTS=false" >> /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfvars
+        echo "CAPACITY_PROVIDER_EXISTS= "false"" >> /var/lib/jenkins/workspace/test/terraform/modules/ecs_config/terraform.tfvars
       fi
     EOT
   }
